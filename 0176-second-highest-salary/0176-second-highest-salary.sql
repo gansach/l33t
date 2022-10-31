f10c8839c68c71ef4ce5de9f563d2d4bfd985c41
Gandharv Sachdeva <53263043+gansach@users.noreply.github.com>
2022-10-31 18:15:47 +0530

    Time: 293 ms (54.03%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,8 +1,2 @@
-SELECT 
-IFNULL(
-    (SELECT DISTINCT salary
-        FROM EMPLOYEE
-    ORDER BY salary DESC
-    LIMIT 1
-    OFFSET 1), NULL
-) AS SecondHighestSalary
+SELECT MAX(salary) AS SecondHighestSalary FROM Employee 
+WHERE salary < (SELECT MAX(salary) FROM Employee)
